faaec48fdae44c65a7ef8b7eb57c72e1453edbd3
andrewitdevlab <103784405+andrewitdevlab@users.noreply.github.com>
2024-03-18 05:06:00 +0000

    Create TestScript.ps1

---
+++ TestFolder/TestScript.ps1
@@ -0,0 +1,6 @@
+$disk = Get-WmiObject Win32_DiskDrive
+$capacity = 0;
+for($i = 0;$i -lt $disk.Count; $i++){
+    $capacity = $capacity + [math]::round(($disk[$i].Size/1GB),2)
+}
+("{0}GB" -f $capacity) | Out-File ("{0}\DiskCapacity.txt" -f [Environment]::GetFolderPath("MyDocuments"))
