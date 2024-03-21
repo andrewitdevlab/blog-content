72e2f12459b700b20d99462f31f02d86f8c889cc
andrewitdevlab <103784405+andrewitdevlab@users.noreply.github.com>
2024-03-21 23:26:09 +0000

    Update TestScript.ps1

--- TestFolder/TestScript.ps1
+++ TestFolder/TestScript.ps1
@@ -3,5 +3,5 @@ $capacity = 0;
 for($i = 0;$i -lt $disk.Count; $i++){
     $capacity = $capacity + [math]::round(($disk[$i].Size/1GB),2)
 }
-#("{0}GB" -f $capacity) | Out-File ("{0}\DiskCapacity.txt" -f [Environment]::GetFolderPath("MyDocuments"))
-("{0}GB" -f $capacity) | Out-File ("{0}\DiskCapacity.txt" -f "C:\Users\SILCOCA0\Documents\POC")
+("{0}GB" -f $capacity) | Out-File ("{0}\DiskCapacity.txt" -f [Environment]::GetFolderPath("MyDocuments"))
+#("{0}GB" -f $capacity) | Out-File ("{0}\DiskCapacity.txt" -f "C:\Users\SILCOCA0\Documents\POC")
